4e40a790202db16cb4eadbdd7ed01b3d6ce64f89
cienna <cienna.dubay@gmail.com>
2017-11-18 19:25:07 -0500

    fix accounts to account

--- lib/capturecampus_web/plugs.ex
+++ lib/capturecampus_web/plugs.ex
@@ -4,7 +4,7 @@ defmodule Capturecampus.Plugs do
   def fetch_user(conn, _opts) do
     user_id = get_session(conn, :user_id)
     if user_id do
-      user = Capturecampus.Accounts.get_user!(user_id)
+      user = Capturecampus.Account.get_user!(user_id)
       assign(conn, :current_user, user)
     else
       assign(conn, :current_user, nil)
