5c7fbea46ba871372669b8e2690529d151778f88
cienna <cienna.dubay@gmail.com>
2017-11-22 11:58:37 -0500

    deploy config

--- config/test.exs
+++ config/test.exs
@@ -12,8 +12,8 @@ config :logger, level: :warn
 # Configure your database
 config :capturecampus, Capturecampus.Repo,
   adapter: Ecto.Adapters.Postgres,
-  username: "postgres",
-  password: "postgres",
+  username: "campus",
+  password: "ahpalah0Xu7u",
   database: "capturecampus_test",
   hostname: "localhost",
   pool: Ecto.Adapters.SQL.Sandbox
