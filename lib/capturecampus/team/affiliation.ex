fe88ce53bd0b215104902ab33c2322ed7aa3c0b9
alshammarif <f.al.shammari995@gmail.com>
2017-11-21 22:02:15 -0500

    ignore

--- lib/capturecampus/team/affiliation.ex
+++ lib/capturecampus/team/affiliation.ex
@@ -6,7 +6,7 @@ defmodule Capturecampus.Team.Affiliation do
 
   schema "affiliations" do
     field :color, :string, null: false
-
+    field :image_path, :string, default: "http://i149.photobucket.com/albums/s71/dylancolesss/white-dot.png", null: false
     timestamps()
   end
 
